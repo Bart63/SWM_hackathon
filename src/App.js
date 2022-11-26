3ec1145c1ad803ed95ab502b5c9c6b27df25f8c7
Konrad Marciniak <konmarciniak@gmail.com>
2022-11-26 17:58:30 +0100

    reading custom histogram based on json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { CustomLineChart } from "./Recharts.js";
 import Example from "./TinyBarChart.js";
 
 export default function MyApp() {
   return (
     <div>
-      <CustomLineChart />
       <Example />
     </div>
 
